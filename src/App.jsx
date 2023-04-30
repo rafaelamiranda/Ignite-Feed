50261a4f639ba852b3c1df99b1b4ae3653a4b712
Rafaela Miranda <rafaela-gomes@outlook.com>
2023-04-30 17:55:33 -0300

    feat: Criação do componente de Post

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import { Header } from './components/Header';
+import { Post } from './components/Post';
 import { Sidebar } from './components/Sidebar';
-import { Post } from './Post';
+
 import styles from './App.module.css';
+
 import './global.css';
 
 function App() {
---
+++ src/components/Post.jsx
@@ -0,0 +1,29 @@
+import styles from './Post.module.css';
+
+export function Post() {
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img className={styles.avatar} src="https://github.com/rafaelamiranda.png" />
+          <div className={styles.authorInfo}>
+            <strong>Rafaela Miranda</strong>
+            <span>Web Developer</span>
+          </div>
+        </div>
+        <time title="30 de Abril às 08:13h" dateTime="2023-04-30 08:13:30">Publicado ha 1h</time>
+      </header>
+
+      <div className={styles.content}>
+      <p>Fala galeraa 👋</p>
+      <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+      <p>👉{' '}<a href=""> jane.design/doctorcare</a></p>
+      <p>
+        <a href="">#novoprojeto</a>{' '}
+        <a href="">#nlw</a>{' '}
+        <a href="">#rocketseat</a>
+      </p>
+      </div>
+    </article>
+  )
+}
