94de4965c77d99be8b926d64f32ff72aa29cac5c
Rafaela Miranda <rafaela-gomes@outlook.com>
2023-04-29 21:02:37 -0300

    feat: Criação da sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,5 @@
+import { PencilLine } from "@phosphor-icons/react";
+
 import styles from './Sidebar.module.css';
 
 export function Sidebar() {
@@ -9,12 +11,15 @@ export function Sidebar() {
       />
 
       <div className={styles.profile}>
+        <img className={styles.avatar} src="https://github.com/rafaelamiranda.png" />
+
         <strong>Rafaela Miranda</strong>
         <span>Web Developer</span>
       </div>
 
       <footer>
         <a href="#">
+          <PencilLine size={20} />
           Editar seu perfil
         </a>
       </footer>
